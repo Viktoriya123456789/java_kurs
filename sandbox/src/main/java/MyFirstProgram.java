9dba237f7322bfd03433843ed5fefee2504552d5
Viktoriya123456789 <wikulja999@yandex.ru>
2017-06-22 14:43:00 +0600

    Использование функции, классы, объекты и методы

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,21 @@
 public class MyFirstProgram {
 
- public static void main(String[] args) {
-   System.out.println("Hello, world!");
- }
+    //public static void main(String[] args) {
 
+    //    System.out.println("Hello, world!");
+// }
+
+    public static void main(String[] args) {
+        Point p1 = new Point(5, 5);
+        Point p2 = new Point(4, 2);
+
+        System.out.println("Расстояние между двумя точками" + "=" +  distance(p1, p2));
+
+    }
+
+
+
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt(((p1.x -p1.y)*(p1.x -p1.y))+((p2.x -p2.y)*(p2.x -p2.y)));
+}
 }
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,15 @@
+/**
+ * Created by admin on 22.06.2017.
+ */
+public class Point {
+
+    public double x;
+    public double y;
+
+
+    public Point (double a, double b){
+        this.x = a;
+        this.y = b;
+    }
+
+}
