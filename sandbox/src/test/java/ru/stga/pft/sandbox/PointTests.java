93803ad8f9b4a72d78fe8104100fd3701c048ab2
Viktoriya123456789 <wikulja999@yandex.ru>
2017-06-23 10:01:29 +0600

    тестовый фреймворк

---
+++ sandbox/src/test/java/ru/stga/pft/sandbox/PointTests.java
@@ -0,0 +1,19 @@
+package ru.stga.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+
+/**
+ * Created by admin on 22.06.2017.
+ */
+public class PointTests {
+
+    @Test
+    public void testDistance() {
+        Point p1 = new Point (5, 5);
+        Point p2 = new Point(4, 2);
+       assert Math.sqrt((((p1.x -p1.y)*(p1.x -p1.y))+((p2.x -p2.y)*(p2.x -p2.y)))) == 2;
+    }
+}
