a56ab125b8d3ac206a103d42172d8679f7887a6f
Viktoriya123456789 <wikulja999@yandex.ru>
2017-06-29 15:17:06 +0600

    ДЗ-7 запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/appmanager/ApplicationManager.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -31,11 +32,11 @@ public class ApplicationManager {
 
 
     public void init() {
-        if (browser == BrowserType.FIREFOX){
+        if (Objects.equals(browser, BrowserType.FIREFOX)){
             wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        } else if (browser == BrowserType.CHROME){
+        } else if (Objects.equals(browser, BrowserType.CHROME)){
             wd = new ChromeDriver();
-        } else if (browser == BrowserType.IE) {
+        } else if (Objects.equals(browser, BrowserType.IE)) {
             wd = new InternetExplorerDriver();
         }
 
---
+++ sandbox/src/test/java/ru/stga/pft/sandbox/Equality.java
@@ -0,0 +1,17 @@
+package ru.stga.pft.sandbox;
+
+/**
+ * Created by admin on 29.06.2017.
+ */
+public class Equality {
+    public static void main (String [] args) {
+        String s1 = "firefox";
+        //String s2 = s1;
+        //String s2 = new String (s1);
+
+        String s2 = "firefox";
+
+        System.out.println (s1 == s2);
+        System.out.println (s1.equals(s2));
+    }
+}
