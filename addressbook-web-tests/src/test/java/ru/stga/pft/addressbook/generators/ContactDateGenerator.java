5ad891f7cc3e002fdc7a6e0ad29bf5209f323095
Viktoriya123456789 <wikulja999@yandex.ru>
2017-07-18 19:22:05 +0600

    закрытие файла Лекция 6.8

--- addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/generators/ContactDateGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/generators/ContactDateGenerator.java
@@ -58,9 +58,9 @@ public class ContactDateGenerator {
     private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
         String json = gson.toJson(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(json);
-        writer.close();
+        try(Writer writer = new FileWriter(file)) {
+            writer.write(json);
+        }
     }
 
 
@@ -68,21 +68,21 @@ public class ContactDateGenerator {
         XStream xStream = new XStream();
         xStream.processAnnotations(ContactData.class);
         String xml = xStream.toXML(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(xml);
-        writer.close();
+        try(Writer writer = new FileWriter(file)) {
+            writer.write(xml);
+        }
     }
 
 
     private void saveAsCsv (List<ContactData> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
-        Writer writer = new FileWriter(file);
-        for (ContactData contact : contacts){
-            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),
-                    contact.getAddress(), contact.getEmail1(), contact.getEmail2(), contact.getEmail3(),
-                    contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone()));
+        try(Writer writer = new FileWriter(file)) {
+            for (ContactData contact : contacts) {
+                writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),
+                        contact.getAddress(), contact.getEmail1(), contact.getEmail2(), contact.getEmail3(),
+                        contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone()));
+            }
         }
-        writer.close();
     }
 
 
