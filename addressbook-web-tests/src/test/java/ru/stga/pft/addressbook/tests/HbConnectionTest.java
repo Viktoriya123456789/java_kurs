c41cbd08180b14162289eae8ec0d346a81932a59
Viktoriya123456789 <wikulja999@yandex.ru>
2017-07-20 19:00:54 +0600

    лекция 7.2 ошибка2

--- addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/tests/HbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/tests/HbConnectionTest.java
@@ -8,6 +8,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.stga.pft.addressbook.model.ContactData;
 import ru.stga.pft.addressbook.model.GroupData;
 
 import java.util.List;
@@ -41,8 +42,22 @@ public class HbConnectionTest {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
         List<GroupData> result = session.createQuery( "from GroupData" ).list();
-        for ( GroupData group : result ) {
+        for (GroupData group : result) {
             System.out.println(group);
+        }
+        session.getTransaction().commit();
+        session.close();
+
+    }
+
+
+    @Test
+    public void testHbConnectionContact(){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<ContactData> result = session.createQuery( "from ContactData" ).list();
+        for (ContactData contact : result) {
+            System.out.println(contact);
         }
         session.getTransaction().commit();
         session.close();
