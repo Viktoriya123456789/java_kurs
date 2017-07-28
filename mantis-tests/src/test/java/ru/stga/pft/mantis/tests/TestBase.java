10c0bcd2c6d969940b2eeec6cef11b77f36016c4
Viktoriya123456789 <wikulja999@yandex.ru>
2017-07-28 10:09:08 +0600

    ДЗ-17 Подготовить новый проект для нового учебного приложения

---
+++ mantis-tests/src/test/java/ru/stga/pft/mantis/tests/TestBase.java
@@ -0,0 +1,26 @@
+package ru.stga.pft.mantis.tests;
+
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stga.pft.mantis.appmanager.ApplicationManager;
+
+/**
+ * Created by admin on 23.06.2017.
+ */
+public class TestBase {
+
+    public static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+}
