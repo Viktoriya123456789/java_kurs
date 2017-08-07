350594cff3e646732a0a7201b8bf769bc631b66d
Viktoriya123456789 <wikulja999@yandex.ru>
2017-08-07 17:31:34 +0600

    ДЗ-19 Реализовать интеграцию тестов с баг-трекером MantisBT

--- mantis-tests/src/test/java/ru/stga/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stga/pft/mantis/tests/TestBase.java
@@ -4,15 +4,13 @@ package ru.stga.pft.mantis.tests;
 import biz.futureware.mantis.rpc.soap.client.IssueData;
 import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
 import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
-import org.hibernate.service.spi.ServiceException;
 import org.openqa.selenium.remote.BrowserType;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stga.pft.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigInteger;
@@ -24,8 +22,6 @@ public class TestBase {
 
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
-
-    Logger loger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite
     public void setUp() throws Exception {
@@ -40,19 +36,19 @@ public class TestBase {
 
     }
 
-    public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException, javax.xml.rpc.ServiceException {
-        if (isIssueOpen(issueId)) {
-            throw new SkipException("Ignored because of issue " + issueId);
-        }
-    }
-
-    public boolean isIssueOpen(int issueId) throws RemoteException, MalformedURLException, ServiceException, javax.xml.rpc.ServiceException {
-        MantisConnectPortType mc = new MantisConnectLocator().getMantisConnectPort(new URL("http://localhost:8080/mantisbt-1.2.19/api/soap/mantisconnect.php"));
+    public boolean isIssueOpen(int issueId) throws RemoteException, MalformedURLException, ServiceException {
+        MantisConnectPortType mc = new MantisConnectLocator().getMantisConnectPort(new URL("http://localhost/mantisbt-1.2.19/api/soap/mantisconnect.php"));
         IssueData fixedIssueData = mc.mc_issue_get("Administrator", "root", BigInteger.valueOf(issueId));
-        if (fixedIssueData.getResolution().getName().equals("fixed")){
+        if (fixedIssueData.getResolution().getName().equals("fixed")) {
             return false;
         } else {
             return true;
+        }
+    }
+
+    public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
         }
     }
 
